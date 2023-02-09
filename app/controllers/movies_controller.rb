9e06ca0ce2aa86be75c952e1412d799f3eb20ef2
Nick Kramer <nick.kramer@shopify.com>
2023-02-09 09:54:18 -0500

    adds show and edit page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,12 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
 end
