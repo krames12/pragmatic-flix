2fd0b13227879df320d39f8ef238650531778346
Nick Kramer <nick.kramer@shopify.com>
2023-02-09 14:10:54 -0500

    moves to partials, adds extra info/images, starts validation

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,6 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+    @movies = Movie.released
   end
 
   def show
@@ -9,5 +9,34 @@ class MoviesController < ApplicationController
 
   def edit
     @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    @movie.update(movie_params)
+    redirect_to @movie
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new
+    @movie.update(movie_params)
+    redirect_to @movie
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_url, status: :see_other
+  end
+
+private
+  def movie_params
+    params.require(:movie).
+      permit(:title, :description, :rating, :released_on, :total_gross,
+              :director, :duration, :image_file_name)
   end
 end
